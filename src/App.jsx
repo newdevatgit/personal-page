3145920614d6e9ad12ce38f3187beb7b165a7eb9
newdevatgit <arpanwar26@gmail.com>
2025-11-10 22:59:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Hello ALL!</h1>
+      </div>
+    </>
+  );
+}
