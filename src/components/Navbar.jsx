bd3a2fc22d062cb3360b4d01b3929056615f5f4f
newdevatgit <arpanwar26@gmail.com>
2025-11-11 17:34:29 +0530

    style: navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar(){
     return (
         <nav>
-        <div className='flex bg-gray-300 justify-between items-center p-6'>
-          <div className='text-2xl font-bold'>
+        <div className='flex bg-gray-300 flex-col gap-2 justify-between items-center p-6'>
+          <div className='text-2xl font-bold border-b-2 border-black b-2'>
             <h1>Personal page</h1>
           </div>
           <div >
